03644226d4b05aae66f4fc69c5e7dc21b4076035
mstojanovic <marko.stojanovic222@gmail.com>
2015-02-11 10:46:35 +0100

    moje izmjene

--- web/exe.php
+++ web/exe.php
@@ -1,6 +1,6 @@
 <?php
 
-echo shell_exec('sh ./KOD/program '.$_POST['funkcija'].' '.$_POST['iteracije'].' '.$_POST['ponavljanja'].'  '.$_POST['n'].' 
-	'.$_POST['omega'].' '.$_POST['ro_p'].' '.$_POST['ro_g'].' '.$_POST['m'].' '.$_POST['e'].'');
-
+$cmd = 'cd KOD ; ./program ' . $_POST['funkcija'] . ' ' . $_POST['iteracije'] . ' ' . $_POST['ponavljanja'] . ' ' . $_POST['n'] . ' ' . $_POST['omega'] . ' ' . $_POST['ro_p'] . ' ' . $_POST['ro_g'] . ' ' . $_POST['m'] . '   ' . $_POST['e']; 
+exec($cmd);
+header('Location: index.php');
 ?>
