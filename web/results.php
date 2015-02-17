1061c9fa49ebabd4f202cb16b0eebb633b3ca359
mstojanovic <marko.stojanovic222@gmail.com>
2015-02-17 16:04:45 +0100

    rezultati tabilca

---
+++ web/results.php
@@ -0,0 +1,8 @@
+<?php
+
+echo ("ABC: <a href='KOD/results/abc.txt'>REZULTATI</a><br />");
+echo ("PSO: <a href='KOD/results/pso.txt'>REZULTATI</a><br />");
+echo ("OPSO: <a href='KOD/results/opso.txt'>REZULTATI</a><br />");
+
+
+?>
