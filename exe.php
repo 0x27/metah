4adf4b0fde350de90c9cd505ad76a3fff845d929
David Davidson <g00271860@gmit.ie>
2016-11-01 09:40:10 +0000

    Fix Command Injection
    Attempt at fixing command injection issues

--- exe.php
+++ exe.php
@@ -1,6 +1,6 @@
 <?php
 
-echo shell_exec('sh ./KOD/program '.$_POST['funkcija'].' '.$_POST['iteracije'].' '.$_POST['ponavljanja'].'  '.$_POST['n'].' 
-	'.$_POST['omega'].' '.$_POST['ro_p'].' '.$_POST['ro_g'].' '.$_POST['m'].' '.$_POST['e'].'');
+echo shell_exec('sh ./KOD/program '.escapeshellarg($_POST['funkcija']).' '.escapeshellarg($_POST['iteracije']).' '.escapeshellarg($_POST['ponavljanja']).'  '.escapeshellarg($_POST['n']).' 
+	'escapeshellarg(.$_POST['omega']).' '.escapeshellarg($_POST['ro_p']).' '.escapeshellarg($_POST['ro_g']).' '.escapeshellarg($_POST['m']).' '.escapeshellarg($_POST['e']).'');
 
 ?>
